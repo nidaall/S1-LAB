183edcf906af8579e660a9f6b1a1bc0128bc9dae
nidal <basilpoul230@gmail.com>
2024-11-29 10:39:12 +0530

    Updated Linklist.c

--- DSA/linklist.c
+++ DSA/linklist.c
@@ -1,36 +1,82 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct node{
+struct node{
     int data;
-    struct node *next;
-}node;
+    struct node* next;
+};
 
-void main(){
-    int n;
-    printf("Enter the limit: ");
-    scanf("%d",&n);
-    struct node* head=NULL,*temp,*current;
-    for(int i=0;i<n;i++){
-        struct node* newnode=(struct node*)malloc(sizeof(node));
-        printf("Enter the data: "); 
-        scanf("%d",&newnode->data);
-        newnode->next=NULL;
-        node* temp=head;
-        if(head==NULL){
-            head=newnode;
-            current = newnode;
-        }
-        else{
-        current->next=newnode;
-        current=newnode;
-        }
+struct node* head=NULL;
+struct node* temp;
+
+void Insert(int data){
+    struct node* newNode=(struct node*)malloc(sizeof(struct node));
+    newNode->data=data;
+    newNode->next=NULL;
+    if (head==NULL){
+        head=newNode;
+        return;
     }
     temp=head;
-    while (temp!=NULL)
-    {
-        printf("%d->",temp->data);
+    while(temp->next!=NULL){
         temp=temp->next;
     }
-    printf("NULL");
+    temp->next=newNode;
+}
+
+void Display(){
+    temp=head;
+    while(temp!=NULL){
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+    printf("\n");
+}
+
+void Delete(int data){
+    struct node *prev = NULL;
+    temp=head;
+    while(temp->data!=data){
+        prev=temp;
+        temp=temp->next;
+        if(data==temp->data)
+        break;
+        if(temp->next==NULL){
+            printf("Element not found\n");
+            return;
+        }
+    }
+    prev->next=temp->next;
+    printf("%d has been deleted\n",temp->data);
+    free(temp);
+}
+
+int main(){
+    int choice,k=1,data,data1;
+    while(k==1){
+        printf("1.Insert\n2.Display\n3.Delete\n4.Exit\n");
+        scanf("%d",&choice);
+        switch (choice)
+        {
+        case 1:
+            printf("Enter data to be inserted: ");
+            scanf("%d",&data);
+            Insert(data);
+            break;
+        case 2:
+            Display();
+            break;
+        case 3:
+            printf("Enter data to be deleted: ");
+            scanf("%d",&data1);
+            Delete(data1);
+            break;
+        case 4:
+            k=0;
+            break;
+        default:
+            printf("Invalid choice\n");
+            break;
+        }
+    }
 }
